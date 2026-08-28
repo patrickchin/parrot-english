86581d2aa2bfe7670d70c69e680b7a95af44fa66
Patrick Chin <p.ch@live.co.uk>
2026-08-28 23:07:35 +0800

    Remove focus bars from reading text

--- src/learner-profile/LearnerProfileLayout.tsx
+++ src/learner-profile/LearnerProfileLayout.tsx
@@ -70,7 +70,7 @@ export function LearnerProfileStepHeading({
     <h1
       {...props}
       className={cx(
-        "relative outline-none before:absolute before:top-0 before:-left-3 before:h-full before:max-h-24 before:w-1 before:transition-none before:content-[''] focus:before:bg-brand-blue forced-colors:before:hidden forced-colors:focus:outline-2 forced-colors:focus:outline-solid forced-colors:focus:outline-offset-2",
+        "outline-none forced-colors:focus:outline-2 forced-colors:focus:outline-solid forced-colors:focus:outline-offset-2",
         className,
       )}
       ref={headingRef}
